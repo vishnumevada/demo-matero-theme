84e94614c01abd1561862fcce35a8282408ccc1d
vishnu mevada <vishnu.mevada@txtech.co>
2024-04-05 11:48:46 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { PreloaderService, SettingsService } from '@core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: '<router-outlet></router-outlet>',
 })
-export class AppComponent {
-  title = 'demo-matero-theme';
+export class AppComponent implements OnInit, AfterViewInit {
+  constructor(
+    private preloader: PreloaderService,
+    private settings: SettingsService
+  ) {}
+
+  ngOnInit() {
+    this.settings.setDirection();
+    this.settings.setTheme();
+  }
+
+  ngAfterViewInit() {
+    this.preloader.hide();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,63 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { CoreModule } from '@core/core.module';
+import { ThemeModule } from '@theme/theme.module';
+import { SharedModule } from '@shared/shared.module';
+import { RoutesModule } from './routes/routes.module';
+import { FormlyConfigModule } from './formly-config.module';
+import { NgxPermissionsModule } from 'ngx-permissions';
+import { ToastrModule } from 'ngx-toastr';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+
+import { environment } from '@env/environment';
+import { BASE_URL, httpInterceptorProviders, appInitializerProviders } from '@core';
+
+// Required for AOT compilation
+export function TranslateHttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
+import { LoginService } from '@core/authentication/login.service';
+import { FakeLoginService } from './fake-login.service';
+
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    CoreModule,
+    ThemeModule,
+    SharedModule,
+    RoutesModule,
+    FormlyConfigModule.forRoot(),
+    NgxPermissionsModule.forRoot(),
+    ToastrModule.forRoot(),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: TranslateHttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    NoopAnimationsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: BASE_URL, useValue: environment.baseUrl },
+    // ==================================================
+    // 👇 ❌ Remove it in the realworld application
+    //
+    { provide: LoginService, useClass: FakeLoginService },
+    //
+    // ==================================================
+    ...httpInterceptorProviders,
+    ...appInitializerProviders,
+  ],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
